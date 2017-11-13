5468dca433553603105c06d30d49094741d80b8b
Lauren Schaefer <lschaefer@sugarcrm.com>
2017-11-13 15:06:24 -0500

    Stick with old pack.php and move functionality to PackageGenerator slowly

--- tests/phpunit/PackageGeneratorTest.php
+++ tests/phpunit/PackageGeneratorTest.php
@@ -7,9 +7,34 @@ use Sugarcrm\ProfessorM\PackageGenerator;
 class PackageGeneratorTest extends TestCase
 {
 
-    public function testPack(){
-        $pack = new PackageGenerator();
-        $this->assertFalse($pack->shouldExcludeFile());
+    public function testShouldIncludeFileInZipValidFileMac(){
+        $pg = new PackageGenerator();
+        $this->assertTrue($pg->shouldIncludeFileInZip("src/custom/Extension/modules/Accounts/Ext/WirelessLayoutdefs/pr_professors_accounts_Accounts.php"));
+    }
+
+    public function testShouldIncludeFileInZipValidFileWindows(){
+        $pg = new PackageGenerator();
+        $this->assertTrue($pg->shouldIncludeFileInZip("src\\custom\\Extension\\modules\\Accounts\\Ext\\WirelessLayoutdefs\\pr_professors_accounts_Accounts.php"));
+    }
+
+    public function testShouldIncludeFileInZipFileInCustomApplicationExtMac(){
+        $pg = new PackageGenerator();
+        $this->assertFalse($pg->shouldIncludeFileInZip("src/custom/application/Ext/test.php"));
+    }
+
+    public function testShouldIncludeFileInZipFileInCustomApplicationExtWindows(){
+        $pg = new PackageGenerator();
+        $this->assertFalse($pg->shouldIncludeFileInZip("src\\custom\\application\\Ext\\test.php"));
+    }
+
+    public function testShouldIncludeFileInZipFileInCustomModulesModuleNameExtMac(){
+        $pg = new PackageGenerator();
+        $this->assertFalse($pg->shouldIncludeFileInZip("src/custom/modules/test/Ext/excludeme.php"));
+    }
+
+    public function testShouldIncludeFileInZipFileInCustomModulesModuleNameExtWindows(){
+        $pg = new PackageGenerator();
+        $this->assertFalse($pg->shouldIncludeFileInZip("src\\custom\\modules\\test\\Ext\\excludeme.php"));
     }
 
 }
