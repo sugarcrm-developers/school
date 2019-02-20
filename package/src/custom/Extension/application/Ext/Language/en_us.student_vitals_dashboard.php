64ee1366b20820a3a778882931d14eb47adebee3
Shad Mickelberry <smickelberry@highlandsolutions.com>
2019-02-20 12:36:17 -0700

    Add missing language file

--- package/src/custom/Extension/application/Ext/Language/en_us.student_vitals_dashboard.php
+++ package/src/custom/Extension/application/Ext/Language/en_us.student_vitals_dashboard.php
@@ -3,3 +3,4 @@
 $app_strings['LBL_STUDENT_VITAL_CHART_DESC'] = 'Pie Chart grouping combined days in vitals status';
 $app_strings['LBL_STUDENT_VITAL_CHART'] = 'Student Vitals Chart';
 $app_strings['LBL_VITALS_DASHLET_SELECT_TEAM'] = 'Select Super Group to view data on';
+$app_strings['LBL_VITALS_DASHLET_SELECT_DATE_RANGE'] = 'Select Date Range';
