914142a884666f83ebb1cada0ec6c934702b812d
mshaheen-sugarcrm <mshaheen@sugarcrm.com>
2019-08-28 15:56:54 -0400

    testing remoevd runpostmantests

--- scripts/SetupEnvAndRunTests.sh
+++ scripts/SetupEnvAndRunTests.sh
@@ -121,7 +121,7 @@ echo "Calling RunProfMPHPUnitTests.sh"
 ./RunProfMPHPUnitTests.sh $sugarDirectory || exit 1
 
 echo "Calling RunPostmanTests.sh"
-./RunPostmanTests.sh $sugarVersion $sugarEdition || exit 1
+#./RunPostmanTests.sh $sugarVersion $sugarEdition || exit 1
 
 echo "Calling StopDockerStack.sh"
 ./StopDockerStack.sh $sugarVersion $sugarDockerDirectory || exit 1
