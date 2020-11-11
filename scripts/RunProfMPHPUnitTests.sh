05eae2f149596f6c8649778777dcd1e8ce9193dd
mshaheen-sugarcrm <mshaheen@sugarcrm.com>
2020-11-11 08:40:21 -0500

    reset composer

--- scripts/RunProfMPHPUnitTests.sh
+++ scripts/RunProfMPHPUnitTests.sh
@@ -14,6 +14,10 @@ docker exec sugar-web1 bash -c "cd tests/unit-php/ && chmod +x ../../vendor/bin/
 ######################################################################
 # Run the Professor M PHPUnit tests
 ######################################################################
+echo "--------------------------"
+node --version
+echo "--------------------------"
+
 
 echo "Running the PHPUnit tests for the Professor M Module Loadable Package..."
 docker exec sugar-web1 bash -c "cd tests/unit-php && ../../vendor/bin/phpunit --testsuite custom"
